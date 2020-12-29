4377b7750eaf44bf8fb5d6386f1a5bf550e5cfe4
LacunaConsultingSTL <75628242+LacunaConsultingSTL@users.noreply.github.com>
2020-12-29 11:44:53 +0500

    Add files via upload

---
+++ build/precache-manifest.08d8653918fb360248c9bbccc5319d08.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1285b30fa6c6e1b5ca0732ebc413b85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0049a02eb30c25b30a8",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "880956e410ffa9736628",
+    "url": "/static/css/main.5250c96b.chunk.css"
+  },
+  {
+    "revision": "b0049a02eb30c25b30a8",
+    "url": "/static/js/2.f5e9f094.chunk.js"
+  },
+  {
+    "revision": "265068e7f3bc904803e029152d67e9a1",
+    "url": "/static/js/2.f5e9f094.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880956e410ffa9736628",
+    "url": "/static/js/main.ec0d2c6e.chunk.js"
+  },
+  {
+    "revision": "5db1d954ada3a6057ef7",
+    "url": "/static/js/runtime-main.7b08ebb7.js"
+  },
+  {
+    "revision": "942c706d6a63231b27dd11f3f2e0c4f0",
+    "url": "/static/media/logo.942c706d.png"
+  }
+]);
