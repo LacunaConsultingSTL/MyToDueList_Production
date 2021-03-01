06ac21951325ddfde4d199b8a7f4f39aa5a56b80
LacunaConsultingSTL <75628242+LacunaConsultingSTL@users.noreply.github.com>
2021-03-01 23:55:16 +0500

    Add files via upload

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6932f3fd3117c7a293bcfaaaae4b575.js"
+  "/precache-manifest.24c6be6d8c8b1d63fae2226b885ea0b3.js"
 );
 
 self.addEventListener('message', (event) => {
