3dc8f1d5a74792b01473f75859c19a1529075c41
LacunaConsultingSTL <75628242+LacunaConsultingSTL@users.noreply.github.com>
2021-03-20 00:10:21 +0500

    Add files via upload

---
+++ build/precache-manifest.8c3ec0bcef71c46b4c9736d894514656.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca0004bf7ecec150003504752e709e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96c60bf19303fc0cd564",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e53fc600651ac73ee467",
+    "url": "/static/css/main.a2dc9b31.chunk.css"
+  },
+  {
+    "revision": "96c60bf19303fc0cd564",
+    "url": "/static/js/2.556bb673.chunk.js"
+  },
+  {
+    "revision": "265068e7f3bc904803e029152d67e9a1",
+    "url": "/static/js/2.556bb673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e53fc600651ac73ee467",
+    "url": "/static/js/main.5da8f510.chunk.js"
+  },
+  {
+    "revision": "d1d7e90d5cf42f9fe5aa",
+    "url": "/static/js/runtime-main.2e45ac1d.js"
+  },
+  {
+    "revision": "942c706d6a63231b27dd11f3f2e0c4f0",
+    "url": "/static/media/logo.942c706d.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23571297e4409fe60c4e44a24c8065c0.js"
+  "/precache-manifest.8c3ec0bcef71c46b4c9736d894514656.js"
 );
 
 self.addEventListener('message', (event) => {
